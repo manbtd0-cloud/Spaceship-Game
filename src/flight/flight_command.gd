ea36cdca013531b8ca0db3e8dd80032055b8bd21
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 04:17:37 +0500

    fix: use named flight mode enum values

--- src/flight/flight_command.gd
+++ src/flight/flight_command.gd
@@ -1,7 +1,7 @@
 class_name FlightCommand
 extends RefCounted
 
-var mode: FlightMode.Value = FlightMode.ASSISTED
+var mode: FlightMode.Value = FlightMode.Value.ASSISTED
 var translation: Vector3 = Vector3.ZERO
 var rotation: Vector3 = Vector3.ZERO
 var boost: float = 0.0
