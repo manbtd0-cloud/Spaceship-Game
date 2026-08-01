cf20241c885082772473ea49e9b3a7a9ac701777
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 04:43:10 +0500

    feat: enter playable flight room from bootstrap

--- src/core/bootstrap.gd
+++ src/core/bootstrap.gd
@@ -1,4 +1,11 @@
 extends Node
 
 func _ready() -> void:
-    print("Shattered Orbit bootstrap ready")
+    call_deferred("_enter_flight_room")
+
+func _enter_flight_room() -> void:
+    var result := get_tree().change_scene_to_file(
+        "res://scenes/flight_room/flight_room.tscn"
+    )
+    if result != OK:
+        push_error("Failed to enter flight room")
