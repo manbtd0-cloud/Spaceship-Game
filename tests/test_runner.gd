a1d1f2ca1ffe130a1271faa8b1e0b2652852a3da
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 04:36:41 +0500

    test: define flight input shaping contract

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -3,6 +3,7 @@ extends SceneTree
 const TEST_SCRIPTS: Array[String] = [
     "res://tests/unit/test_test_harness.gd",
     "res://tests/unit/test_flight_model.gd",
+    "res://tests/unit/test_player_input_math.gd",
 ]
 
 func _initialize() -> void:
