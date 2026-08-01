044537d032e584ffffed614678c8fb088a9452f2
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 04:38:42 +0500

    test: register playable interceptor contract

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -5,6 +5,7 @@ const TEST_SCRIPTS: Array[String] = [
     "res://tests/unit/test_flight_model.gd",
     "res://tests/unit/test_player_input_math.gd",
     "res://tests/unit/test_ship_flight_state.gd",
+    "res://tests/integration/test_player_scene.gd",
 ]
 
 func _initialize() -> void:
