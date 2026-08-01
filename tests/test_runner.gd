e6e39c205af558d68b1f634225c38de2af944110
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 03:06:14 +0500

    test: register pure flight model suite

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -2,6 +2,7 @@ extends SceneTree
 
 const TEST_SCRIPTS: Array[String] = [
     "res://tests/unit/test_test_harness.gd",
+    "res://tests/unit/test_flight_model.gd",
 ]
 
 func _initialize() -> void:
