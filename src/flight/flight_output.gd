efa14e1675f88e453116cf0fca4d18745f80f91a
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 20:50:07 +0500

    feat: split pilot and assisted flight output

--- src/flight/flight_output.gd
+++ src/flight/flight_output.gd
@@ -1,5 +1,13 @@
 class_name FlightOutput
 extends RefCounted
 
+var pilot_force_local: Vector3 = Vector3.ZERO
+var pilot_torque_local: Vector3 = Vector3.ZERO
+var assist_force_local: Vector3 = Vector3.ZERO
+var assist_torque_local: Vector3 = Vector3.ZERO
 var force_local: Vector3 = Vector3.ZERO
 var torque_local: Vector3 = Vector3.ZERO
+
+func finalize_totals() -> void:
+    force_local = pilot_force_local + assist_force_local
+    torque_local = pilot_torque_local + assist_torque_local
