e92c56111671a50e8d4a84a9af58669875bdd287
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 05:42:12 +0500

    feat: integrate thermal boost state

--- src/player/ship_flight_controller.gd
+++ src/player/ship_flight_controller.gd
@@ -12,6 +12,8 @@ var _body: RigidBody3D
 var _input_source: PlayerInputSource
 var _flight_mode: FlightMode.Value = FlightMode.Value.ASSISTED
 var _boost_amount := 0.0
+var _boost_heat := 0.0
+var _boost_locked_out := false
 var _local_velocity := Vector3.ZERO
 
 func _ready() -> void:
@@ -30,7 +32,7 @@ func _ready() -> void:
         push_error("ShipFlightController requires a FlightTuning resource")
         set_physics_process(false)
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
     if _input_source.consume_capture_toggle():
         _input_source.set_mouse_captured(not _input_source.is_mouse_captured())
 
@@ -42,7 +44,20 @@ func _physics_process(_delta: float) -> void:
         reset_requested.emit()
 
     var command := _input_source.sample_command(_flight_mode)
-    _boost_amount = command.boost
+    var thermal_state := BoostThermalState.advance(
+        _boost_heat,
+        _boost_locked_out,
+        command.boost,
+        command.translation.length(),
+        delta,
+        tuning.boost_heat_per_second,
+        tuning.boost_cooling_per_second,
+        tuning.boost_recovery_threshold
+    )
+    _boost_heat = thermal_state.heat
+    _boost_locked_out = thermal_state.locked_out
+    _boost_amount = thermal_state.effective_boost
+    command.boost = _boost_amount
 
     var basis := _body.global_transform.basis.orthonormalized()
     var local_linear := basis.inverse() * _body.linear_velocity
@@ -53,7 +68,8 @@ func _physics_process(_delta: float) -> void:
         command,
         tuning,
         local_linear,
-        local_angular
+        local_angular,
+        _body.mass
     )
     _body.apply_central_force(basis * output.force_local)
     _body.apply_torque(basis * output.torque_local)
@@ -67,6 +83,27 @@ func get_speed_mps() -> float:
 func get_boost_amount() -> float:
     return _boost_amount
 
+func get_boost_heat() -> float:
+    return _boost_heat
+
+func is_boost_locked_out() -> bool:
+    return _boost_locked_out
+
+func get_boost_recovery_progress() -> float:
+    return BoostThermalState.recovery_progress(
+        _boost_heat,
+        tuning.boost_recovery_threshold
+    ) if tuning != null else 0.0
+
+func get_active_speed_limit() -> float:
+    if tuning == null:
+        return 0.0
+    return (
+        tuning.boost_speed_limit
+        if _boost_amount > 0.0
+        else tuning.normal_speed_limit
+    )
+
 func get_local_velocity() -> Vector3:
     return _local_velocity
 
@@ -75,3 +112,8 @@ func get_world_velocity() -> Vector3:
 
 func get_body() -> RigidBody3D:
     return _body
+
+func reset_runtime_state() -> void:
+    _boost_heat = 0.0
+    _boost_locked_out = false
+    _boost_amount = 0.0
