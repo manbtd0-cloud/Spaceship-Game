1a8ce206462ef8eda6f26e2a4e4356412f0d422f
manbtd0-cloud <manbtd0@gmail.com>
2026-08-02 15:22:31 +0500

    test: run canonical fighter and thruster suites

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -9,13 +9,14 @@ const TEST_SCRIPTS: Array[String] = [
     "res://tests/unit/test_coordinated_turn_state.gd",
     "res://tests/unit/test_player_input_math.gd",
     "res://tests/unit/test_ship_flight_state.gd",
-    "res://tests/unit/test_hero_ship_alignment.gd",
-    "res://tests/unit/test_ship_visual_math.gd",
+    "res://tests/unit/test_ship_flight_controller_state.gd",
+    "res://tests/unit/test_ship_thruster_allocator.gd",
+    "res://tests/unit/test_thruster_exhaust_effect.gd",
     "res://tests/integration/test_player_scene.gd",
     "res://tests/unit/test_chase_camera_math.gd",
     "res://tests/integration/test_flight_room_scene.gd",
     "res://tests/integration/test_hero_ship_asset.gd",
-    "res://tests/integration/test_hero_ship_adapter.gd",
+    "res://tests/integration/test_ship_thruster_visual_controller.gd",
 ]
 
 func _initialize() -> void:
