6005f6e38fe3ded815ae0467d95c8fcff8aa102c
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 00:15:10 +0500

    test: require logical primary fire action

--- tests/integration/test_input_map.gd
+++ tests/integration/test_input_map.gd
@@ -1,6 +1,33 @@
 extends "res://tests/support/test_case.gd"
 
+const REQUIRED_ACTIONS: Array[StringName] = [
+    &"thrust_forward",
+    &"thrust_reverse",
+    &"strafe_left",
+    &"strafe_right",
+    &"strafe_up",
+    &"strafe_down",
+    &"pitch_up",
+    &"pitch_down",
+    &"yaw_left",
+    &"yaw_right",
+    &"roll_left",
+    &"roll_right",
+    &"boost",
+    &"fire_primary",
+    &"toggle_flight_mode",
+    &"toggle_mouse_capture",
+    &"reset_flight_room",
+    &"camera_cycle",
+]
+
 func run() -> void:
+    for action: StringName in REQUIRED_ACTIONS:
+        assert_true(
+            InputMap.has_action(action),
+            "required input action missing: %s" % action
+        )
+
     assert_equal(
         _physical_key_for_action(&"pitch_up"),
         KEY_DOWN,
@@ -16,15 +43,29 @@ func run() -> void:
         KEY_C,
         "C must cycle chase-camera presets"
     )
+    assert_equal(
+        _physical_key_for_action(&"fire_primary"),
+        KEY_V,
+        "physical V must trigger the logical primary fire action"
+    )
 
     var c_owners := 0
+    var v_owners := 0
     for action: StringName in InputMap.get_actions():
-        if _physical_key_for_action(action) == KEY_C:
+        var physical_key := _physical_key_for_action(action)
+        if physical_key == KEY_C:
             c_owners += 1
+        if physical_key == KEY_V:
+            v_owners += 1
     assert_equal(
         c_owners,
         1,
         "physical C must belong only to camera_cycle"
+    )
+    assert_equal(
+        v_owners,
+        1,
+        "physical V must belong only to fire_primary"
     )
 
 func _physical_key_for_action(action: StringName) -> int:
