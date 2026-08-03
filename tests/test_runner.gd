50d140a577bc204df4eea16ea68882ceb4b4f3cc
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 00:29:48 +0500

    test: register reusable damage state suite

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -9,6 +9,7 @@ const TEST_SCRIPTS: Array[String] = [
     "res://tests/unit/test_coordinated_turn_state.gd",
     "res://tests/unit/test_player_input_math.gd",
     "res://tests/unit/test_primary_fire_cadence.gd",
+    "res://tests/unit/test_damage_state.gd",
     "res://tests/integration/test_input_map.gd",
     "res://tests/integration/test_primary_fire_input.gd",
     "res://tests/unit/test_ship_flight_state.gd",
