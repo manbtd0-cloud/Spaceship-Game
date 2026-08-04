71868699dbe31c05dd39e71bd745f0dcfc88f408
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 19:04:42 +0500

    test: register camera behavior contracts

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -23,6 +23,7 @@ const TEST_SCRIPTS: Array[String] = [
     "res://tests/unit/test_asteroid_field_layout.gd",
     "res://tests/integration/test_player_scene.gd",
     "res://tests/unit/test_chase_camera_math.gd",
+    "res://tests/integration/test_camera_behavior_modes.gd",
     "res://tests/integration/test_chase_camera_rig.gd",
     "res://tests/integration/test_flight_room_scene.gd",
     "res://tests/integration/test_hero_ship_asset.gd",
