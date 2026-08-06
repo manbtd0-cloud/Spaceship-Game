3bb3498e8d22b269e85ba92f4a3813ce2dfdbd31
manbtd0-cloud <manbtd0@gmail.com>
2026-08-06 21:09:22 +0500

    feat: add practice drone tuning type

---
+++ src/combat/practice_drone_tuning.gd
@@ -0,0 +1,14 @@
+class_name PracticeDroneTuning
+extends Resource
+
+@export var preferred_distance: float = 140.0
+@export var distance_band: float = 25.0
+@export var maximum_speed: float = 45.0
+@export var maximum_acceleration: float = 18.0
+@export var orbit_speed: float = 24.0
+@export var radial_speed: float = 30.0
+@export var vertical_speed: float = 8.0
+@export var velocity_response: float = 1.8
+@export var maximum_turn_torque: float = 12000.0
+@export var angular_damping: float = 3000.0
+@export var respawn_delay: float = 3.0
